0e0ba4c8f7355e6c2337a0675152003481706933
himanshunetinnovatus <himanshu.netinnovatus@gmail.com>
2025-05-26 00:21:17 +0530

    5 26 25

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -12,13 +12,105 @@ const create = async (req, res) => {
         });
         await user.save();
 
-        res.status(201).json({ message: "User create successfully", user });
+        return res.status(201).json({ message: "User create successfully", user });
     } catch (err) {
         console.log(err);
-        res.status(400).json({ error: error.message });
+        return res.status(400).json({ error: error.message });
     }
 }
 
+const findbyid = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const userData = await User.findById({ _id: id })
+        if (!userData) {
+            return res.status(404).json({ msg: "User not exist" })
+        }
+        return res.status(200).json(userData);
+        // return res.send(200).json({ msg: "User not exist", usre: userData })
+    } catch (err) {
+        return res.status(500).json({ error: err.message });
+    }
+}
+
+const findAll = async (req, res) => {
+
+    try {
+        // const userData = await User.find({}); // it is returing all of the documents
+        // const userData = await User.find({ city: "chd" }, { name: 1, _id: 0, city: 1 }).skip(5); // with filter and proection
+        // const userData = await User.find({ city: "chd" }, { name: 1, _id: 0, city: 1 }).limit(4); // with filter and proection
+        // const userData = await User.find({ city: "chd" }, { name: 1, _id: 0, city: 1 }).limit(0).sort({ name: -1 }); // with filter and proection
+        // const userData = await User.find({ city: "chd" }, { name: 1, _id: 0, city: 1 }).limit(0).sort({ name: -1 }); // with filter and proection
+        // const userData = await User.find({ city: "chd" }).select('-_id'); // with for include and exclude
+        // const userData = await User.find({ city: "chd" }).lean(); // return js JS objects instead of Mongoose docs
+        // const userData = await User.find({ phone: {$gte : 1212121212, $lte : 5555555555 }  }); // return js JS objects instead of Mongoose docs
+        // const userData = await User.find({ phone: { $eq: 1212121212 } }); 
+        // const userData = await User.find({ phone: { $ne: 1212121212 } }); 
+        // const userData = await User.find({ phone: { $in: [1212121212, 212123234] } }); 
+        // const userData = await User.find({ phone: { $nin: [1212121212, 212123234] } }); 
+        // const userData = await User.find( { $or: [{ phone: 1212121212 }, { phone: 212123234 }] }); 
+        // const userData = await User.find( { $and: [{ phone:{ $gte : 1212121212 }}, { phone:{ $lte :5555555555 }}] }); 
+        // const userData = await User.find( { $and: [{ phone:{ $gte : 1212121212 }}, { phone:{ $lte :5555555555 }}] });  
+        // const userData = await User.find({ phone: { $not: { $gte: 5555555555 } } }); 
+        const userData = await User.find({ phone: { $exists: true } });
+
+        if (!userData.length === 0) {
+            return res.status(404).json({ msg: "User not exist" })
+        }
+        return res.status(201).json(userData);
+        // return res.send(200).json({ msg: "User not exist", usre: userData })
+    } catch (err) {
+        return res.status(500).json({ error: err.message });
+    }
+}
+
+const upadateByid = async (req, res) => {
+    const { id } = req.params;
+    const { name, email } = req.body;
+
+    try {
+
+        const userData = await User.findByIdAndUpdate({ _id: id },
+            {
+                name: name,
+                email: email
+            }, { new: true }
+        )
+        if (!userData) {
+            return res.status(404).json({ msg: "User not exist" })
+        }
+        return res.status(201).json(userData);
+        // return res.send(200).json({ msg: "User not exist", usre: userData })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: err.message });
+    }
+}
+
+const upadateMany = async (req, res) => {
+    const { id } = req.params;
+    const { name, email } = req.body;
+
+    try {
+
+        const userData = await User.findByIdAndUpdate({ _id: id },
+            {
+                name: name,
+                email: email
+            }, { new: true }
+        )
+        if (!userData) {
+            return res.status(404).json({ msg: "User not exist" })
+        }
+        return res.status(201).json(userData);
+        // return res.send(200).json({ msg: "User not exist", usre: userData })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: err.message });
+    }
+}
+
+
 module.exports = {
-    create
+    create, findbyid, findAll, upadateByid, upadateMany
 }
--- router/user.router.js
+++ router/user.router.js
@@ -5,9 +5,13 @@ const userRouter = express.Router();
 
 
 const {
-    create 
+    create , findbyid, findAll, upadateByid, upadateMany
 } = require("../controller/user.controller")
 
 userRouter.post("/create", create) // http://localhost:3000/user/create
+userRouter.get("/findbyid/:id", findbyid) // http://localhost:3000/user/create
+userRouter.get("/findall", findAll) // http://localhost:3000/user/findall
+userRouter.post("/fidupdate/:id", upadateByid) // http://localhost:3000/user/findall
+userRouter.post("/updatemany/:id", upadateMany) // http://localhost:3000/user/findall
 
 module.exports = userRouter;
